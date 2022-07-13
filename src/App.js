2d0c28600351ffa20c0fc6f8f8521645ebb218f3
donnelworks <donnel.works@gmail.com>
2022-07-13 17:30:48 +0700

    input contact

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import ContactInput from "./components/ContactInput";
 import ContactList from "./components/ContactList";
 import { getData } from "./data";
 
 function App() {
-  const contacts = getData();
+  const [contacts, setContacts] = useState(getData());
+
+  const onSubmit = ({ name, tag }) => {
+    setContacts([
+      ...contacts,
+      { id: +new Date(), name, tag, img: "/images/default.jpg" },
+    ]);
+    console.log(contacts);
+  };
   return (
     <div className="container">
-      <h1 className="title">Daftar Kontak</h1>
+      <h1 className="title">Aplikasi Kontak</h1>
+      <h2 className="subtitle">Tambah Kontak</h2>
+      <ContactInput dataSubmit={(data) => onSubmit(data)} />
+      <h2 className="subtitle">Daftar Kontak</h2>
       <ContactList contacts={contacts} />
     </div>
   );
---
+++ src/components/ContactInput.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function ContactInput({ dataSubmit }) {
+  const [form, setForm] = useState({ name: "", tag: "" });
+
+  const onChangeData = (val, input) => {
+    setForm({
+      ...form,
+      [input]: val.target.value,
+    });
+  };
+
+  const onSubmitData = (e) => {
+    e.preventDefault();
+    dataSubmit(form);
+  };
+  return (
+    <div className="input-container">
+      <form onSubmit={(e) => onSubmitData(e)}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          value={form.name}
+          onChange={(val) => onChangeData(val, "name")}
+        />
+        <label htmlFor="tag">Tag</label>
+        <input
+          type="text"
+          id="tag"
+          value={form.tag}
+          onChange={(val) => onChangeData(val, "tag")}
+        />
+        <button type="submit" className="btn-submit">
+          Tambah
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default ContactInput;
